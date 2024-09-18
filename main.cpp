8d659878a6428140ae8a200438c4430becaac74c
Marth1nus <tinus1.prinsloo@gmail.com>
2024-09-18 04:06:06 +0200

    WebGL

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <print>
 #include <vector>
 #include <array>
 #include <ranges>
@@ -6,8 +5,21 @@
 #include <algorithm>
 #include <stdexcept>
 
+#include <cstdio>
+#include <string>
+#include <string_view>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <GLES3/gl3.h>
+#include <GLFW/glfw3.h>
+#else
 #include <glad/gles2.h>
 #include <GLFW/glfw3.h>
+#endif
+
+auto static constexpr gol_step_dt = 1.0 / 30.0;
+auto static constexpr gol_width = 512, gol_height = gol_width;
 
 auto static constexpr &&vert_code{R"glsl(
   #version 300 es
@@ -71,23 +83,7 @@ auto inline static constexpr assert(auto &&value, auto &&message) -> decltype(au
 }
 #define ASSERT(...) assert(__VA_ARGS__, #__VA_ARGS__)
 
-struct glfw
-{
-  glfw() { ASSERT(glfwInit()); }
-  ~glfw() { glfwTerminate(); }
-} static const glfw{};
-
-auto const window = []
-{
-  auto const width = 720, height = width;
-  auto const window = ASSERT(glfwCreateWindow(width, height, "Game Of Life", 0, 0));
-  glfwMakeContextCurrent(window);
-  ASSERT(gladLoadGLES2(glfwGetProcAddress));
-  glViewport(0, 0, width, height);
-  return window;
-}();
-
-auto static constexpr glCheckError() noexcept -> void
+auto static glCheckError()
 {
   for (GLenum err; (err = glGetError()) not_eq GL_NO_ERROR;)
   {
@@ -100,71 +96,70 @@ auto static constexpr glCheckError() noexcept -> void
       default:                               return "UNKNOWN_ERROR"; } }(); /* clang-format on */
     std::fprintf(stderr, "OpenGL ES error 0x%x: %s\n", err, str);
   }
-};
+}
 
-auto static constexpr get_info_log(decltype(glGetShaderiv) get_iv,
-                                   decltype(glGetShaderInfoLog) get_info_log,
-                                   GLuint id, GLenum pname) noexcept -> std::string
+struct glfw
+{
+  glfw() { ASSERT(glfwInit()); }
+  ~glfw() { glfwTerminate(); }
+} static const glfw{};
+
+auto static window_lifetime = []
+{
+  auto const width = 720, height = width;
+  auto const window_lifetime = std::shared_ptr<GLFWwindow>{
+      ASSERT(glfwCreateWindow(width, height, "Game Of Life", 0, 0)), glfwDestroyWindow};
+  auto const window = window_lifetime.get();
+  glfwMakeContextCurrent(window);
+#ifdef __EMSCRIPTEN__
+#else
+  ASSERT(gladLoadGLES2(glfwGetProcAddress));
+#endif
+  glViewport(0, 0, width, height);
+  return window_lifetime;
+}();
+auto static window = window_lifetime.get();
+
+auto static get_info_log(decltype(glGetShaderiv) get_iv, decltype(glGetShaderInfoLog) get_info_log, GLuint id, GLenum pname) noexcept -> std::string
 {
   std::string res{};
   if (GLint status; get_iv(id, pname, &status), status)
     return res;
   GLsizei len;
-  res.resize_and_overwrite(
-      (get_iv(id, GL_INFO_LOG_LENGTH, &len), len),
-      [&](char *buf, size_t) -> size_t
-      { return get_info_log(id, len, &len, buf), len; });
+  res.resize((get_iv(id, GL_INFO_LOG_LENGTH, &len), len));
+  res.resize((get_info_log(id, len, &len, res.data()), len));
   return res;
-};
+}
 
-auto static constexpr load_shader(GLenum type, std::string_view code) noexcept -> GLuint
+auto static load_shader(GLenum type, std::string_view code) -> GLuint
 {
+  auto version_offset = code.find("#version");
+  if (version_offset == std::string_view::npos or version_offset >= code.size())
+    throw std::runtime_error{"Shader did not start with #version"};
   auto sid = glCreateShader(type);
-  auto str = code.data();
-  auto len = static_cast<GLsizei>(code.size());
+  auto str = code.data() + version_offset;
+  auto len = static_cast<GLsizei>(code.size() - version_offset);
   glShaderSource(sid, 1, &str, &len);
   glCompileShader(sid);
   if (auto log = get_info_log(glGetShaderiv, glGetShaderInfoLog, sid, GL_COMPILE_STATUS); not log.empty())
     std::fprintf(stderr, "Shader Compiler Error: \n%s\n", log.c_str());
   glCheckError();
   return sid;
-};
+}
 
-GLuint static pid = glCreateProgram();
+auto pid = glCreateProgram();
 struct
 {
   GLint tex, gol_step;
 } uniforms;
 
-auto static reload_program(char const *vert = vert_code,
-                           char const *frag = frag_code)
+auto reload_program(char const *vert = vert_code, char const *frag = frag_code)
 {
   glDeleteProgram(pid);
   pid = glCreateProgram();
   GLuint vid, fid;
-  if constexpr (0) /* From are Files or Embedded */
-  {
-    for (auto [id, path, type] : {std::tuple{&vid, vert, GL_VERTEX_SHADER},
-                                  std::tuple{&fid, frag, GL_FRAGMENT_SHADER}})
-    {
-      std::string code;
-      if (auto file = std::fopen(path, "r"))
-      {
-        std::fseek(file, 0, SEEK_END);
-        auto len = std::ftell(file);
-        std::fseek(file, 0, SEEK_SET);
-        code.resize_and_overwrite(len, [&](char *buf, size_t len) -> size_t
-                                  { return std::fread(buf, sizeof(buf[0]), len, file); });
-        std::fclose(file);
-      }
-      *id = load_shader(type, code);
-    }
-  }
-  else
-  {
-    vid = load_shader(GL_VERTEX_SHADER, vert);
-    fid = load_shader(GL_FRAGMENT_SHADER, frag);
-  }
+  vid = load_shader(GL_VERTEX_SHADER, vert);
+  fid = load_shader(GL_FRAGMENT_SHADER, frag);
   glAttachShader(pid, vid);
   glAttachShader(pid, fid);
   glLinkProgram(pid);
@@ -176,7 +171,7 @@ auto static reload_program(char const *vert = vert_code,
   uniforms.tex /*      */ = glGetUniformLocation(pid, "tex");
   uniforms.gol_step /* */ = glGetUniformLocation(pid, "gol_step");
   glCheckError();
-};
+}
 
 auto static constexpr vertices = std::array{
     std::array{-1.0f, -1.0f},
@@ -206,43 +201,57 @@ auto static vao = []
   return vao;
 }();
 
-auto static reload_texture(int width = 512, int height = -1)
+auto reload_texture(GLuint tid, int width = gol_width, int height = gol_height)
 {
-  height = height < 0 ? width : height;
   std::vector<uint8_t> pixels(width * height * sizeof(uint8_t));
-  std::ranges::generate(pixels, std::rand);
+  for (auto &pixel : pixels)
+    pixel = std::rand();
+  glBindTexture(GL_TEXTURE_2D, tid);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, pixels.data());
   glCheckError();
-};
+}
 
-auto static tid = []
+auto static tids = []
 {
-  GLuint tid;
-  glGenTextures(1, &tid);
-  glBindTexture(GL_TEXTURE_2D, tid);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  reload_texture();
-  return tid;
+  std::array<GLuint, 2> tids;
+  glGenTextures(tids.size(), tids.data());
+  for (auto tid : tids)
+  {
+    glBindTexture(GL_TEXTURE_2D, tid);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    reload_texture(tid);
+  }
+  return tids;
 }();
 
-auto static fbo = []
+auto static fbos = []
 {
-  GLuint fbo;
-  glGenFramebuffers(1, &fbo);
-  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tid, 0);
-  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) not_eq GL_FRAMEBUFFER_COMPLETE)
-    throw std::runtime_error{"Framebuffer is incomplete!\n"};
+  std::array<GLuint, 2> fbos;
+  glGenFramebuffers(fbos.size(), fbos.data());
+  for (auto i = 0; i < fbos.size(); i++)
+  {
+    glBindFramebuffer(GL_FRAMEBUFFER, fbos.at(i));
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tids.at(i), 0);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) not_eq GL_FRAMEBUFFER_COMPLETE)
+      throw std::runtime_error{"Framebuffer is incomplete!\n"};
+    glCheckError();
+  }
+  return fbos;
+}();
+
+auto static dt_stamp = glfwGetTime(), gol_stamp = dt_stamp;
+
+auto static inline setup()
+{
+  reload_program();
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glCheckError();
-  return fbo;
-}();
+}
 
-double static dt_stamp = glfwGetTime(), gol_stamp = dt_stamp;
-
-auto static inline loop() -> void
+auto static inline loop()
 {
   auto dt_stamp_old = dt_stamp;
   dt_stamp = glfwGetTime();
@@ -252,16 +261,20 @@ auto static inline loop() -> void
   glUseProgram(pid);
   glBindVertexArray(vao);
 
-  if (dt_stamp - gol_stamp > (1.0 / 64))
+  if (dt_stamp - gol_stamp > gol_step_dt)
   {
     gol_stamp = dt_stamp;
     reload_program();
+    std::swap(tids.at(0), tids.at(1));
+    std::swap(fbos.at(0), fbos.at(1));
     glUniform1i(uniforms.gol_step, 1);
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindTexture(GL_TEXTURE_2D, tids.at(1));
+    glBindFramebuffer(GL_FRAMEBUFFER, fbos.at(0));
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glUniform1i(uniforms.gol_step, 0);
   }
 
+  glUniform1i(uniforms.gol_step, 0);
+  glBindTexture(GL_TEXTURE_2D, tids.at(0));
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
   glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
@@ -269,19 +282,15 @@ auto static inline loop() -> void
   glfwSwapBuffers(window);
 }
 
-auto static inline setup() -> void
-{
-  reload_program();
-  glCheckError();
-
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  glfwSwapInterval(1);
-  while (not glfwWindowShouldClose(window))
-    loop();
-}
-
 int main()
 {
   setup();
-  loop();
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(loop, 0, true);
+  glfwSwapInterval(1);
+#else
+  glfwSwapInterval(1);
+  while (not glfwWindowShouldClose(window))
+    loop();
+#endif
 }
